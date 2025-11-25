63575580f11ac00e7d9d77acc25d4a8d2061ba11
elcurras <alejandrovillarsevilla@gmail.com>
2025-11-25 18:24:55 +0100

    config inicial

---
+++ configVars.js
@@ -0,0 +1,10 @@
+export default {
+    db: {
+        name: 'db1',
+        username: 'remoto',
+        password: 'MiPasswordSeguro123',
+        host: '51.49.113.234',
+        port: 3306,
+        dialect: 'mariadb'
+    }
+};
